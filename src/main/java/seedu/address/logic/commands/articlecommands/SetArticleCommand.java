c1ca026a4a6b71b5056f30198dd28234e44f7c6c
Ko-Khan <e0970313@u.nus.edu>
2024-03-28 13:50:04 +0800

    Add new SetCommand class

--- src/main/java/seedu/address/logic/commands/articlecommands/SetArticleCommand.java
+++ src/main/java/seedu/address/logic/commands/articlecommands/SetArticleCommand.java
@@ -4,4 +4,9 @@ package seedu.address.logic.commands.articlecommands;
  * Use to set filter for Articles
  */
 public class SetArticleCommand {
+    public static final String COMMAND_WORD = "list";
+
+    public static final String COMMAND_PREFIX = "-a";
+
+    public static final String MESSAGE_SUCCESS = "Filter Updated";
 }
--- src/main/java/seedu/address/model/ArticleFilter.java
+++ src/main/java/seedu/address/model/ArticleFilter.java
@@ -1,8 +1,20 @@
 package seedu.address.model;
 
+import java.util.function.Predicate;
+
+import seedu.address.model.article.Article;
+
 /**
  * Use to filter through ArticleBook
  */
-public class ArticleFilter implements Filter{
-
+public class ArticleFilter implements Filter {
+    private Predicate<Article> statusPredicate;
+    public void setStatusPredicate(String s) {
+        statusPredicate = new Predicate<Article>() {
+            @Override
+            public boolean test(Article article) {
+                return article.getStatus().toString().equals(s);
+            }
+        };
+    }
 }
