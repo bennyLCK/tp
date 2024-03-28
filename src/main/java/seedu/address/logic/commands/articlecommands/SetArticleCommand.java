7d1e94ea5087ac3519171871646b535eee84d4b1
Ko-Khan <e0970313@u.nus.edu>
2024-03-28 15:16:22 +0800

    Fix Documentation
    Add ArticleMatchesStatusPredicate
    class.
    
    Add exception for invalid statuses.

--- src/main/java/seedu/address/logic/commands/articlecommands/SetArticleCommand.java
+++ src/main/java/seedu/address/logic/commands/articlecommands/SetArticleCommand.java
@@ -1,12 +1,25 @@
 package seedu.address.logic.commands.articlecommands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_ARTICLES;
+
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.model.Model;
+
 /**
  * Use to set filter for Articles
  */
-public class SetArticleCommand {
-    public static final String COMMAND_WORD = "list";
+public class SetArticleCommand extends ArticleCommand {
+    public static final String COMMAND_WORD = "set";
 
     public static final String COMMAND_PREFIX = "-a";
 
     public static final String MESSAGE_SUCCESS = "Filter Updated";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredArticleList(PREDICATE_SHOW_ALL_ARTICLES);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
 }
---
+++ src/main/java/seedu/address/logic/parser/SetArticleCommandParser.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.articlecommands.SetArticleCommand;
+
+/**
+ * Parses input arguments and creates a SetArticleCommand object
+ */
+public class SetArticleCommandParser implements Parser<SetArticleCommand> {
+
+}
