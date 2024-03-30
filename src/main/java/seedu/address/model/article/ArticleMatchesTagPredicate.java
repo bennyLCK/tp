f11736cf99775ca7724fd73391fd57744ca53337
Ko-Khan <e0970313@u.nus.edu>
2024-03-30 15:37:15 +0800

    Add new predicate

---
+++ src/main/java/seedu/address/model/article/ArticleMatchesTagPredicate.java
@@ -0,0 +1,32 @@
+package seedu.address.model.article;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Set;
+import java.util.function.Predicate;
+
+import seedu.address.model.tag.Tag;
+
+/**
+ * Checks if article has matching tag
+ */
+public class ArticleMatchesTagPredicate implements Predicate<Article> {
+    private Tag tag;
+    public ArticleMatchesTagPredicate(Tag tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public boolean test(Article article) {
+        Set<Tag> others = article.getTags();
+        boolean predicate = false;
+        requireNonNull(tag);
+        for (Tag other : others) {
+            requireNonNull(other);
+            if (other.equals(tag)) {
+                predicate = true;
+            }
+        }
+        return predicate;
+    }
+}
