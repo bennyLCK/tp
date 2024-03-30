02ba69c6d906da9e65a62c8f8aed1c90947de82d
Ko-Khan <e0970313@u.nus.edu>
2024-03-30 21:15:26 +0800

    Replace prefix tag with article tag prefix

--- src/main/java/seedu/address/logic/parser/FilterArticleCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterArticleCommandParser.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ARTICLETAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_END;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_START;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.stream.Stream;
 
@@ -18,12 +18,12 @@ public class FilterArticleCommandParser implements Parser<FilterArticleCommand> 
     @Override
     public FilterArticleCommand parse(String userInput) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(userInput, PREFIX_STATUS,
-                PREFIX_TAG, PREFIX_START, PREFIX_END);
-        if (!arePrefixesPresent(argMultimap, PREFIX_STATUS, PREFIX_TAG, PREFIX_START, PREFIX_END)) {
+                PREFIX_ARTICLETAG, PREFIX_START, PREFIX_END);
+        if (!arePrefixesPresent(argMultimap, PREFIX_STATUS, PREFIX_ARTICLETAG, PREFIX_START, PREFIX_END)) {
             throw new ParseException("The filter command does not follow the correct format");
         }
         String status = argMultimap.getValue(PREFIX_STATUS).get();
-        String tagName = argMultimap.getValue(PREFIX_TAG).get();
+        String tagName = argMultimap.getValue(PREFIX_ARTICLETAG).get();
         String start = argMultimap.getValue(PREFIX_START).get();
         String end = argMultimap.getValue(PREFIX_END).get();
         return new FilterArticleCommand(status, tagName, start, end);
