3c7a986e96981e0ab84da1b4f09ed6b022264bc8
H4mes <123077808+H4mes@users.noreply.github.com>
2024-04-04 23:12:05 +0800

    Refactor prefix names

--- src/main/java/seedu/address/logic/commands/articlecommands/AddArticleCommand.java
+++ src/main/java/seedu/address/logic/commands/articlecommands/AddArticleCommand.java
@@ -1,13 +1,13 @@
 package seedu.address.logic.commands.articlecommands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_AUTHOR;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CONTRIBUTOR;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_HEADLINE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERVIEWEE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_OUTLET;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PUBLICATION_DATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_SOURCE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TITLE;
 
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
@@ -29,18 +29,18 @@ public class AddArticleCommand extends ArticleCommand {
     public static final String MESSAGE_USAGE = COMMAND_WORD + " " + COMMAND_PREFIX
             + ": Adds an article to the article book.\n"
             + "Parameters: "
-            + PREFIX_TITLE + "HEADLINE "
-            + "[" + PREFIX_AUTHOR + "CONTRIBUTOR]... "
-            + PREFIX_PUBLICATION_DATE + "DATE] "
-            + "[" + PREFIX_SOURCE + "INTERVIEWEE]... "
+            + PREFIX_HEADLINE + "HEADLINE "
+            + "[" + PREFIX_CONTRIBUTOR + "CONTRIBUTOR]... "
+            + PREFIX_DATE + "DATE] "
+            + "[" + PREFIX_INTERVIEWEE + "INTERVIEWEE]... "
             + "[" + PREFIX_TAG + "TAG]... "
             + "[" + PREFIX_OUTLET + "OUTLET]... "
             + PREFIX_STATUS + "STATUS\n"
             + "Example: " + COMMAND_WORD + " " + COMMAND_PREFIX + " "
-            + PREFIX_TITLE + "The Great Article "
-            + PREFIX_AUTHOR + "John Doe "
-            + PREFIX_PUBLICATION_DATE + "2021-10-10 "
-            + PREFIX_SOURCE + "Jane Doe "
+            + PREFIX_HEADLINE + "The Great Article "
+            + PREFIX_CONTRIBUTOR + "John Doe "
+            + PREFIX_DATE + "2021-10-10 "
+            + PREFIX_INTERVIEWEE + "Jane Doe "
             + PREFIX_TAG + "friends "
             + PREFIX_OUTLET + "The Great Outlet "
             + PREFIX_STATUS + "DRAFT";
