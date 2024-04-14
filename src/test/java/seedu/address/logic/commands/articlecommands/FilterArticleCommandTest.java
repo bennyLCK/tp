436e1dbb853302fe296f1dae5317d8adab3b1dfd
Ko-Khan <e0970313@u.nus.edu>
2024-04-14 11:56:31 +0800

    Fix test

--- src/test/java/seedu/address/logic/commands/articlecommands/FilterArticleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/articlecommands/FilterArticleCommandTest.java
@@ -2,14 +2,14 @@ package seedu.address.logic.commands.articlecommands;
 
 
 import static seedu.address.logic.commands.articlecommands.ArticleCommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalArticles.getTypicalArticleBook;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.ArticleBook;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -24,8 +24,8 @@ public class FilterArticleCommandTest {
     private Model expectedModel;
     @BeforeEach
     public void setUp() {
-        model = new ModelManager(getTypicalAddressBook(), new ArticleBook(), new UserPrefs());
-        expectedModel = new ModelManager(model.getAddressBook(), new ArticleBook(), new UserPrefs());
+        model = new ModelManager(new AddressBook(), getTypicalArticleBook(), new UserPrefs());
+        expectedModel = new ModelManager(new AddressBook(), model.getArticleBook(), new UserPrefs());
     }
     @Test
     public void execute_zeroParameters_showUnfilteredList() throws ParseException {
